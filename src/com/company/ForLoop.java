5be13da34549869e108b1f806eed73999eec620d
Aneebch <aneeb.chatha786@gmail.com>
2023-03-05 13:17:07 +0100

    ForLoop

---
+++ src/com/company/ForLoop.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " Hello world");
+            if(){
+
+            }
+        }
+        System.out.println("fin");
+    }
+}
