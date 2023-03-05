5d35ea674f09af97aa0f1a5ef3613f7d6ef50e95
Aneebch <aneeb.chatha786@gmail.com>
2023-03-05 13:48:26 +0100

    Estructure control

---
+++ src/com/company/Coche.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Coche {
+
+    //atributes
+    String color;
+    String fabricante;
+    Double peso;
+    String modelo;
+    Double largo;
+    Integer velocidad;
+
+    //constructors
+    public Coche(String color,String fabricante,Double peso,String modelo,Double largo) {
+        this.color = color;
+        this.fabricante = fabricante;
+        this.peso = peso;
+        this.modelo = modelo;
+        this.largo = largo;
+
+    }
+
+    // comportamiento
+
+    public void acelerar(Integer cantidad){
+        if(cantidad > 0 && cantidad < 120){
+        this.velocidad += cantidad;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "color='" + color + '\'' +
+                ", fabricante='" + fabricante + '\'' +
+                ", peso=" + peso +
+                ", modelo='" + modelo + '\'' +
+                ", largo=" + largo +
+                ", velocidad=" + velocidad +
+                '}';
+    }
+}
