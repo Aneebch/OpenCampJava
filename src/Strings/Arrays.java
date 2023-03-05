f602028c9a98999f02ce9e88e203ae444d099547
Aneebch <aneeb.chatha786@gmail.com>
2023-03-05 15:31:05 +0100

    Arrays in java

---
+++ src/Strings/Arrays.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] notas = {0, 5, 8, 10, 9, 8, 7, 2, 9, 8};
+
+/*        for(int i = 0; i < notas.length; i++){
+            System.out.println(notas[i]);
+        }
+
+        for (int item: notas) {
+            System.out.println(item);
+        }*/
+
+//        notas.binarySearch();
+//       notas.sort(notas);
+        // equals();
+    }
+}
